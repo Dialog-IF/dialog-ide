eeabb33e87f25de529608b9f0724147870bac689
Howard Lewis Ship <hlship@gmail.com>
2026-08-07 22:33:25 -0700

    chore: untrack dist/ build output
    dist/ is already in .gitignore but was checked in at some point; stop
    tracking it so the workspace doesn't show local rebuilds as dirty.

--- dist/dialoged/skein/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './process';
-export * from './session';
-export * from './tree';
-export * from './service';
-export * from './dynamic';
-export * from './persistence';
-export * from './io';
-//# sourceMappingURL=index.d.ts.map
